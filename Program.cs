cd6e349ddd381e956fb2d83e23935858271aefe2
bbright7 <123539698+bbright7@users.noreply.github.com>
2023-03-13 16:27:00 +0700

    exam

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class program {
+    static void Main(string[] args) {
+        Console.WriteLine("grabfood delivery");
+        Console.WriteLine("Please select menu");
+        Console.WriteLine("-------------");
+       Console.WriteLine("1. Breakfast Set");
+       Console.WriteLine("2. Weekend Set");
+       Console.WriteLine("3. coffee");
+       Console.Write("Please enter a valid menu: ");
+       string input = Console.ReadLine();
+       switch(input) {
+        case "Breakfast Set": 
+            Console.Clear();
+            
+            Console.WriteLine("Ok Please Wait^^");
+            break;
+        case "Weekend Set":
+            Console.Clear();
+            Console.WriteLine("Weekend Set");
+            break;
+        case "coffee": 
+            Console.Clear();
+            Console.WriteLine("Sorry your order is out of stock TT");
+            break;
+        default: 
+            Console.WriteLine("End");
+            break;
+       }
+
+    }
+   
+    
+}
+ 
+ 
+  
